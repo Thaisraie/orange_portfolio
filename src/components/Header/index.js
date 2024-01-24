40cbff05fca373ec88ea24b9d1adb3b68d6d9472
Thais Siqueira <thaisdivino21@gmail.com>
2024-01-24 00:18:08 -0300

    criando cabeçalho

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import iconHeader from '../../assets/img/icon-header.png'
+import iconProfile from '../../assets/img/icon-profile.png'
+import iconNotification from '../../assets/img/icon-notification.png'
+import './styles.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img className='header-icon' src={iconHeader} alt="ícone"/>
+            <div className='container-title' >
+            <h1 className='header-title'>Meus Projetos</h1>
+            <h1 className='header-title'>Descobrir</h1>
+            </div>
+            <div className='container-icons' >
+            <img className='profile-icon'  src={iconProfile} alt='ícone perfil'/>
+            <img src={iconNotification} alt='ícone notificação'/>
+            </div>
+        </div>
+    )
+}
+
+export default Header
